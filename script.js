b3df269e22fe87814c0117f0021a8acac0406e12
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2025-04-04 14:01:25 -0400

    created a border for the table

--- script.js
+++ script.js
@@ -9,64 +9,29 @@ let table = document.getElementById("table");
 
 let reset = document.getElementById("resetBtn");
 
-let column = [];
-let row = [];
-
-// function createTable(amount) {
-//     for(i = 1; i <= amount; i++) {
-//         let newRow = document.createElement("tr");
-//      for (j = 1; j <= amount; j++) {
-//         let column = document.createElement("td");
-//         column.innerHTML = i + "," + j;
-//         newRow.innerHTML += column.outerHTML;
-//      }
-//      table.innerHTML += newRow.outerHTML;
-//     }
-// }
-
-// function updateTable(event) {
-//     event.preventDefault();
-//     if (input.value == " ") {
-//         table.innerHTML = " ";
-//         createTable(input.value);
-//     }
-// }
-
-// form.addEventListener("submit", function (event) {
-//     updateTable(event);
-// })
-
-// window.addEventListener("load", function() {
-//     createTable(15);
-// })
-
-
-
 window.addEventListener("load", () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    for (let i = 1; i <= input.value; i++) {
+      let tr = document.createElement("tr");
+      for (let j = 1; j <= input.value; j++) {
+        let td = document.createElement("td");
+        td.textContent = `${i}  ${j}`;
 
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        for(let i = 1; i <= input.value; i++) {
-           let tr = document.createElement("tr");
-           for(let j = 1; j <= input.value; j++) {
-          let td = document.createElement("td");
-          td.textContent = `${i} ${j}`
-          tr.append(td);
-           }
-          table.append(tr);
+        if ((i + j) % 2 === 0) {
+          td.style.background = "#ccc";
+        } else {
+          td.style.background = "#0000";
         }
-        
-    
-    
-    })
-
-   
-})
-
+        tr.append(td);
+      }
+      table.append(tr);
+    }
+  });
+});
 
 reset.addEventListener("click", (e) => {
-    e.preventDefault();
-    form.reset();
-    table.textContent = "";
-} )
+  e.preventDefault();
+  form.reset();
+  table.textContent = "";
+});
